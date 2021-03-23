9b0242d0270484b68798e4fd902655f3b6be3db5
DaniielaRodas <79351969+DaniielaRodas@users.noreply.github.com>
2021-03-23 11:18:13 -0600

    Update main.cpp
    se creo el programa de comparación de asistentes en las carreras de ingeniería para saber cual usara el salón mayor

--- main.cpp
+++ main.cpp
@@ -2,6 +2,32 @@
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+  // Daniela Rodas - 1652421 
+#include <iostream>
+
+using namespace std;
+
+int main () {
+ 
+	int C1, C2, C3;
+	cout << "Ingrese la cantidad de asistentes para Ingenieria en sistemas:";
+	cin >> C1;
+	cout << "Ingrese la cantidad de asistentes para Ingenieria Industrial:";
+	cin >> C2;
+	cout << "Ingrese la cantidad de asistentes para Ingenieria Civil:";
+	cin >> C3;
+
+	if (C1 > C2 && C1 > C3) {
+		cout << "A la carrera de Ingenieria en sistemas se le asignara el salon mayor";
+	}
+	else if (C2 > C1 && C2 > C3) {
+		cout << "A la carrera de Ingenieria Industrial se le asignara el salon mayor";
+	}
+	else if (C3 > C1 && C3 > C2) {
+		cout << "A la carrera de Ingenieria Civil se le asignara el salon mayor ";
+	}
+
+}
   std::cin.ignore();
   return 0;
 }
