6635522fd86b175f6a21fed332178e66dc5b963b
Alejandro Yaquian <67024148+Jays1043@users.noreply.github.com>
2021-03-23 10:05:33 -0600

    Josue Alejandro Yaquian Sarceño
    Pull request

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PropuestaSimposio
  Evaluación parcial de introducción a la ingeniería en informática y sistemas
+Programa.
+El programa al ejecutarse,
+pide al usario que ingrese la cantidad de asistentes en cada carrera.
+Despues se realiza un if que busara cal es el mayor numero.
+Luego se imprime en pantalla.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+//Josue Alejandro Yaquian Sarceño - 2350721
 #include <iostream>
-int main()
-{
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+using namespace std;
+int main(){
+
+  int sis, civ, ind;
+
+  cout << "Asistentes de Ingenieria en Sistemas: ";
+  cin >> sis;
+  cout << "Asistentes de Ingenieria Civil: ";
+  cin >> civ;
+  cout << "Asistentes de Ingenieria Industrial: ";
+  cin >> ind;
+
+  if (sis > civ && ind){
+    cout << "El salon mayor es para: Ingenieria en sistemas.";
+  }
+  else if (civ > sis && ind){
+    cout << "El salon mayor es para: Ingenieria civil.";
+  }
+  else if (ind > sis && civ){
+    cout << "El salon mayor es para: Ingenieria industrial.";
+  }
   std::cin.ignore();
   return 0;
 }
