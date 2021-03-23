5cd781a5f375ac227b790c81937c03129fe66d3d
Javier Sánchez <78811548+JavierSanchez17@users.noreply.github.com>
2021-03-23 10:06:49 -0600

    Cambios_De_PropuestaSimposio
    Se agregaro un nuevo programa al simposio en el cual se puede verificar a que ingenieria se le asignara el salon mas grande

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 int main()
 {
+   int a, b, c;
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+   
+
+  cout<<"Ingrese la cantidad de estudiantes de la carrera de ingenieria en sistemas"<<endl;cin>>a;cout<<endl;
+  cout<<"Ingrese la cantidad de estudiantes de la carrera de ingenieria industrial"<<endl;cin>>b;cout<<endl;
+  cout<<"Ingrese la cantidad de estudiantes de la carrera de ingenieria civil"<<endl;cin>>c;cout<<endl;
+  if (a>b & b>c){
+    cout<<"El salon mayor se le asignara a la carrera de ingenieria en sistemas"<<endl;
+  }
+  else if (b>a & b>c){
+    cout<<"El salon mayor se le asignara a la carrera de ingenieria industrial"<<endl;
+  }
+  else{
+    cout<<"El salon mayor se le asignara a la carrera de ingenieria civil"<<endl;
+  }
   std::cin.ignore();
   return 0;
 }
