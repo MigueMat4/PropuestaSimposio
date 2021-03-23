bb00774cbf93ac6f734563b47f34fc95078f5105
unknown <karlselizabethlopez@gmail.com>
2021-03-23 10:10:00 -0600

    Programa de conferencistas
    Este es un programa que permite verificar las edades de los conferencistas.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+//	KARLA ELIZABETH LOPEZS AVILA- 1510421
 #include <iostream>
 int main()
 {
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
+  // PROGRAMA PARA CONFERENCISTA JOVENES
+	int C1, C2;
+	char N1, N2;
+	Cout << "Ingrese el nombre del primer conferencista" >> ;
+		cint N1
+	Cout << "Ingrese el nombre del segundo conferencista" >> ;
+		cint N2
+	Cout << "Ingrese la edad del primer conferencista" >> ;
+		cint C1
+	Cout << "Ingrese la edad del segundo conferencista" >> ;
+		cint C1
+
+
   return 0;
 }
