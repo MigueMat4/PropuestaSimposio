9538a4550af87c135572a10f5ad1a02ca6274bae
JEREMY MERIDA <78811460+shimbito@users.noreply.github.com>
2021-03-23 09:52:29 -0600

    miprogramitaxd
    hice el programa asignado por el ingeniero.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+using namespace std;
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
+
+  char nombre1, nombre2;
+  int edad1, edad2;
+
+  cout << "bienvenidos a la encuesta" << endl;
+  cout << "Primerparticipante "<< endl;
+  cout << "Ingrese su nombre: ";
+  cin >> nombre1;
+  cout << "ingrese su edad: ";
+  cin >> edad1;
+  cout << "segundo participante"<< endl;
+  cout << "Ingrese su nombre: ";
+  cin >> nombre2;
+  cout << "ingrese su edad: ";
+  cin >> edad2;
+
+  if (edad1 > edad2)
+  {
+    cout << "felicidades " << nombre1 << "eres mas alto que el enano de " << nombre2 << endl;
+  }
+  else
+  {
+    cout << "felicidades " << nombre2 << "eres mas alto que el enano de " << nombre1 << endl;
+  }
   return 0;
 }
