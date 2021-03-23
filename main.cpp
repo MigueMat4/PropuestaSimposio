15aa15ed699e01f6a8cdaab74004b0d4fbe22f0c
Leonela Motta <69704985+Leonela11@users.noreply.github.com>
2021-03-23 10:06:14 -0600

    Parcial Leonela
    Parcial de introduccion a la ingenieria en sistemas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+//Britany Leonela Alvarez Motta
 #include <iostream>
 int main()
 {
+  
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
   return 0;
