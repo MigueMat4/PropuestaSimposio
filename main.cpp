cc8684fd91ed8535a9832f55f37c31b8e7b39568
sebaspaf10 <79324003+sebaspaf10@users.noreply.github.com>
2021-03-23 10:37:42 -0600

    Programa Para el salon mayor

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+// Sebastian Pereira - 
 #include <iostream>
 int main()
 {
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
-  return 0;
+	int x, y, x;
+	cout << "Cuantos asistentes habran en Civil" << endl;
+		cin >> x;
+	cout << "Cuantos asistentes habran en sistemas" << endl;
+		cin >> y;
+	cout << "Cuantos asistentes habran en Industrial" << endl;
+		cin >> z;
+
+
+	if (x > y && x > z)
+	{
+		cout << "A civil se le asignara el salon mayor" << endl;
+	}
+	if (y >x && y > z)
+	{
+		cout << "A Sistemas se le asignara el salon mayor" << endl;
+	}
+	if (z > y && z > x)
+	{
+		cout << "A Industrial se le asignara el salon mayor" << endl;
+	}
 }
