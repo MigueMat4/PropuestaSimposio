e0549017a6868866a1e3176122dc5db1e3a512bf
Leonela Motta <69704985+Leonela11@users.noreply.github.com>
2021-03-23 10:29:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,27 @@
 #include <iostream>
 int main()
 {
-  
+  int estudiantes1, estudiantes2, estudiantes3;
+  int mayor;
+  cout << "ingrese la cantidad de estudiantes de la carrera";
+ cin >> estudiantes
+ mayor = 0;
+ If (estudiantes1 > mayor){
+mayor = estudiantes1;
+ else if (estudiantes2> mayor){
+ mayor = estudiantes2;
+ cout << "Ingrese la cantidad de estudiantes de la carrera 1";
+ cout << "Ingrese la cantidad de estudiantes de la carrera 1";
+cin >> estudiantes1;
+ }
+if (estudiantes1>estudiantes2){
+  cout << "Ingrese la cantidad de los estudiantes integrantes al simposio";
+  cin >> estudiantes2;
+
+}
+{ if (estudiantes 1 > estudiantes 3)
+{ cout << "se le da el salon a los estudiantes de la carrear 1"} 
+}
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
   return 0;
