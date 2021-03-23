225d664e40bf636a9d979a4fe9b716e06757925c
Angel André Cotoc Cuzal <78944937+AngelAndreCotocCuzal@users.noreply.github.com>
2021-03-23 09:43:43 -0600

    gh repo clone AngelAndreCotocCuzal/PropuestaSimposio

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+// Angel Cotoc 1518321
+
 #include <iostream>
+
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
   return 0;
+// "ls" no me dejo crear las carpetas para ignorar pero asumo qeu existe una llamada .gitattributes
+ 
 }
