bf052dbd74c670468e698c51ee8ba22ed1f9fb1f
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2021-03-23 09:58:34 -0600

    cambios en programa
    se establecio todos los cambios en el programa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+// Ruben Fernando Espinoza Fernandez - 1525620
 #include <iostream>
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+  cout << "Bienvenido\nIngrese la cantidad de asistentes del simposio"
+  
+  
+  
+  
+  
+  
+  
   std::cin.ignore();
   return 0;
 }
