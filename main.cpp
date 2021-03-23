ee3ce25e94cb036855513e02a5f381c858a4434a
MARROQUIN1106 <78808339+MARROQUIN1106@users.noreply.github.com>
2021-03-23 10:09:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+//Juan Diego Marroquín Escobar 1689821
 #include <iostream>
 int main()
 {
+	
+	int confe1, confe2;
+	cout << "Ingrese la edad del primer conferencista" << endl;
+	cin >> confe1;
+	cout << "Ingrese la edad del segundo conferencista" << endl;
+	cin >> confe2;
+
+	if (confe1 > cofe2) 
+	{
+		cout << "Conferencista 2 es el mas joven" << endl;
+	}
+	else 
+	{
+		cout << "Conferencista 1 es el mas joven" << endl;
+	}
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
   return 0;
