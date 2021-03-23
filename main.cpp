051bbf15c8cbf02424ac7dd2363d579d8b5c14e3
NerySarat <78806551+NerySarat@users.noreply.github.com>
2021-03-23 10:18:05 -0600

    Cambio en el codigo
    Nery Sarat

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+// Nery Daniel Sarat Az - 1599421
 #include <iostream>
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
+  std::cout << "Ingrese la cantidad de estudiantes de ingenieria en sistemas \n ";
+  std::cout << "Ingrese la cantidad de estudiantes de ingenieria Industrial \n ";
+  std::cout << "Ingrese la cantidad de estudiantes de ingenieria civil \n ";
+  std::cin.ignore(main.exe);
   return 0;
 }
