2e7a3cc8c359b50c336fe5b560dd75ff293de2d6
PabloCoti <78811893+PabloCoti@users.noreply.github.com>
2021-03-23 09:56:14 -0600

    Programa de estudiante 15-30
    Programa en el que se ingresa el número de participantes del simposio y, si el número es múltiplo de 4 es virtual, de lo contrario es presencial.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
+//Pablo Andrés Cotí Arredondo 1653221
+
 #include <iostream>
-int main()
-{
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
+using namespace std;
+int main(){
+  cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+  int students;
+  cout << "Ingrese la cantidad de participantes que asistirán al simposio: ";
+  cin >> students;
+  if((students%4) == 0){
+    cout << "El simposio debe ser de modalidad virual.";
+  } else {cout << "El simposio debe ser en modalidad presencial";}
   return 0;
 }
