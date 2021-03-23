a973bf51ce1e1c93ddcb6fe6e3caa30a70648f18
Diego Alejandro Navichoque Pérez <78981597+DANavi721@users.noreply.github.com>
2021-03-23 10:00:15 -0600

    DiegoParcial
    ingrese 2 edades, y el programa dirá quien es el más joven

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+//Diego Alejandro Navichoque Perez - 1663721
 #include <iostream>
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
+  string talker[2]; int age[2]; bool check;
+  for (int i = 0; i < 2; i++) {
+      cout << "Ingrese el nombre de su conferecista " << i + 1 << ": "; cin >> talker[i];
+      do {
+          cout << "\nIngrese la edad de su conferencista " << i + 1 << ": "; cin >> age[i];
+          if (age[i] <= 0) {
+              cout << "Debe ser una edad real!\n";
+              check = false;
+          }
+          else { check = true; }
+      } while (check == false);
   return 0;
 }
