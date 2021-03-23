64e3d0ae9b9d343f3823a2bd6ac9ab0b48e7b482
Fernanda Galvez <78811360+fmgalvez@users.noreply.github.com>
2021-03-23 10:07:33 -0600

    Programa C++
    Programa de propuesta simposio

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+//Fernanda Maria Galvez Cifuentes carnet: 1635421
 #include <iostream>
+using namespace std;
+
 int main()
 {
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
-  return 0;
+	//Propuesta de programa para el simposio de Ingenieria 2021
+	int num;
+	cout << "Cuanto estudiantes van a participar en el simposio?" << endl;
+	cin >> num;
+
+	if (num % 4 == 0) {
+		cout << "Lo estara haciendo de manera virtual" << endl;
+	}
+	else
+	{
+		cout << "Lo estara haciendo de manera presencial" << endl;
+	}
+
+	system("Pause")
 }
