2eee496e2e74269bf48d84e326f5b16e0f5a751a
kaneky08s <78807012+Kanek08@users.noreply.github.com>
2021-03-23 09:57:15 -0600

    el programa
    se añadio un programa para edades de 2 conferensistas

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
+      // Keneth Kanek Lopez Perez - 1649121
 #include <iostream>
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+      int joven;
+      int joven2;
+      cout << "ingrese la edad del primer conferensista"<<endl;
+      cin >> joven;
+      cout << "ingrese la edad del segundo conferensista"<<endl;
+      cin >> joven2;
+      if (joven > joven2){
+        cout << "es mas joven el primer conferensista"<<joven <<endl;
+      } else (joven2 > joven){
+        cout "es mas joven el conferensista 2"<< joven2<<endl;      }
   std::cin.ignore();
   return 0;
 }
