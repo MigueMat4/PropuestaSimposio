a44291c3fcccdb197ac9b5e05135302be73bfa95
Juan Marroquin <65145980+Juan520dlc@users.noreply.github.com>
2021-03-23 10:05:35 -0600

    Programa que determina quien de los 2 conferencistas es el más jóven.
    Al principio se le pide al programa que agregue las variables "nombre1" y "nombre2" para solicitar los nombres de los conferencistas, después se le pide al usuario ingresar el nombre y edad de los conferencistas. Al final, el programa mostrará el nombre del conferencista más jóven.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+//Juan Antonio Marroquin Reyes - 1510521
 #include <iostream>
+#include <cstring>
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+
+  string nombre1 = nombre1, nombre2 = nombre2;
+  int edad1, edad2;
+    std::cout << "Por favor, ingrese el nombre del conferencista 1:\n";
+    std::cin >> nombre1;
+    std::cout << "Ahora ingrese la edad del conferencista 1:\n";
+    std::cin >> edad1;
+    std::cout << "Por favor, ingrese el nombre del conferencista 2:\n";
+    std::cin >> nombre2;
+    std::cout << "Ahora ingrese la edad del conferencista 2:\n";
+    std::cin >> edad2;
+
+    if (edad1 < edad2) {
+      std::cout << nombre1 << "es el conferencista mas joven\n";
+    }
+    else {
+      std::cout << nombre2 << "es el conferencista mas joven\n";
+    }
+
   std::cin.ignore();
   return 0;
 }
