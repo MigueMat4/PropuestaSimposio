1ae50bb143bff3d328c3150f3f06e2e604a69f90
Dickson-Cotom <78808712+Dickson-Cotom@users.noreply.github.com>
2021-03-23 10:22:56 -0600

    Programa Parcial
    EL programa que pidio el inge

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 int main()
+int estudiantes;
+
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
+  cout << "Ingrese la cantidad de estudiantes";
+  cin >> estudiantes;
+  if (estudiantes % 4 == 0) {
+	  cout << "presencial";
+  }
+  else { cout << "virtual"; }
   return 0;
 }
